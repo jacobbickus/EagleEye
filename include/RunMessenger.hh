398056ebc546a6d1f729ce49400fe042d715de7d
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2020-10-13 02:24:45 -0400

    Update RunMessenger.hh

--- include/RunMessenger.hh
+++ include/RunMessenger.hh
@@ -1,1 +1,27 @@
+#ifndef RunMessenger_h
+#define RunMessenger_h 1
 
+#include "globals.hh"
+#include "G4UImessenger.hh"
+#include "G4UIcmdWithAString.hh"
+#include "RunAction.hh"
+#include "G4UIdirectory.hh"
+
+class RunAction;
+class G4UIcmdWithAString;
+class G4UIdirectory;
+
+class RunMessenger: public G4UImessenger
+{
+public:
+  RunMessenger(RunAction*);
+  ~RunMessenger();
+
+  void SetNewValue(G4UIcommand*, G4String);
+private:
+  RunAction* runA;
+  G4UIcmdWithAString* Cmd;
+  G4UIdirectory *myDir;
+};
+
+#endif
