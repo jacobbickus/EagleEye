6d49fb447f665a2faa825d1354211fc0b547e23b
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2020-10-13 02:22:33 -0400

    Update RunMessenger.cc

--- src/RunMessenger.cc
+++ src/RunMessenger.cc
@@ -1,1 +1,33 @@
+#include "RunMessenger.hh"
 
+
+RunMessenger::RunMessenger(RunAction* run)
+:runA(run)
+{
+  myDir = new G4UIdirectory("/myrun/");
+  myDir->SetGuidance("Run Commands");
+  Cmd = new G4UIcmdWithAString("/myrun/filename",this);
+  Cmd->SetGuidance("Choose Desired Run Output filename");
+  Cmd->SetParameterName("filename",false);
+  Cmd->SetDefaultValue("Test");
+
+}
+
+RunMessenger::~RunMessenger()
+{
+  delete Cmd;
+}
+
+
+void RunMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
+{
+  if(command == Cmd)
+  {
+    G4String theCommand = newValue;
+    runA->SetFilename(theCommand);
+  }
+  else
+  {
+    G4cerr << "ERROR RunMessenger:: SetStepInputValue command != Cmd" << G4endl;
+  }
+}
