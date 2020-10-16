2a0d087cb0805ce178ceddced9d8885d4b8e6132
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2020-10-16 01:59:12 -0400

    Update RunAction.cc

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -36,11 +36,11 @@ void RunAction::BeginOfRunAction(const G4Run*)
 void RunAction::EndOfRunAction(const G4Run*)
 {
   std::cout << "RunAction::EndOfRunAction" << std::endl;
-   G4int rank = G4MPImanager::GetManager()-> GetRank();
+   //G4int rank = G4MPImanager::GetManager()-> GetRank();
   //NOTE: if only histograms are active actually we do not create the per-thread
   //ntuple file
-  std::ostringstream fname;
-  fname<<thefilename<<rank<<"-thread"<<G4Threading::G4GetThreadId();
-  Analysis* myana = Analysis::GetAnalysis();
-  myana-> Save(fname.str());
+  //std::ostringstream fname;
+  //fname<<thefilename<<rank<<"-thread"<<G4Threading::G4GetThreadId();
+  //Analysis* myana = Analysis::GetAnalysis();
+  //myana-> Save(fname.str());
 }
