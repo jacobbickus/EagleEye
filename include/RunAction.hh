2865d7ecd8970ca7dc2dfa3b7bfde46788e4e3d6
Jacob Bickus <62081787+jacobbickus@users.noreply.github.com>
2020-10-15 22:06:59 -0400

    Update RunAction.hh

--- include/RunAction.hh
+++ include/RunAction.hh
@@ -14,9 +14,16 @@ public:
   virtual G4Run* GenerateRun();
   virtual void BeginOfRunAction(const G4Run* arun);
   virtual void EndOfRunAction(const G4Run* arun);
+  
+  void SetFilename(G4String val)
+    {
+      thefilename = val;
+      std::cout << "Filename set to: " << thefilename << std::endl;
+    }
 
 private:
   RunMessenger* runM;
+  G4String thefilename;
 };
 
 #endif
